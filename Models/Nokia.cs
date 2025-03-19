4ffcf786f254409fbe2a7126de62952830efe4b7
vmt <viniciusmattheus@outlook.com>
2025-03-19 20:31:10 -0300

    feat: implementação da classe

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,14 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
+        public Nokia(string numero) : base(numero) {}
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o {nomeApp} no Nokia.....");
+        }
     }
 }
